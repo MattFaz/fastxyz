e0106e0924183a964edcc6108cfe421970c5215e
Matt Farrell <mfarrell@squareup.com>
2025-10-14 08:59:21 +1000

    basic auth

--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
 const fastify = require("fastify")({
   logger: true,
+  routerOptions: {
+    ignoreTrailingSlash: true,
+  },
 });
 const { version } = require("../package.json");
 const { initStockPriceScheduler } = require("./scheduler/stockPriceFetcher");
+
+// Global auth hook
+fastify.addHook("preHandler", (request, reply, done) => {
+  const apiKey = request.headers["X-API-KEY"];
+  if (apiKey !== process.env.API_KEY) {
+    return reply.code(401).send({ error: "Unauthorized" });
+  }
+  done();
+});
 
 // Register plugins and routes
 const registerModules = async () => {
   await fastify.register(require("./plugins/env.js"));
   await fastify.register(require("@fastify/cors"), {
     methods: ["GET"],
+    origin: "*",
   });
   await fastify.register(require("./routes/price.js"));
 };
